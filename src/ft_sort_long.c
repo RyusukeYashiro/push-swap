1e6118583de9a908f2cc50c759b3ceedbae96c8b
ryusukeyasiro <ryusukeyasiro@gmail.com>
2024-06-09 01:33:36 +0900

    more then 7

---
+++ src/ft_sort_long.c
@@ -0,0 +1,17 @@
+#include "push_swap.h";
+
+void ft_sort_min(t_stack a , t_stack b , t_node **array , int count)
+{
+    if(count == 2)
+    {
+
+    }
+    else if(count == 3)
+    {
+
+    }
+    else if(count == 6)
+    {
+
+    }
+}
