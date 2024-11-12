8ea33766d761861543e653b95c2acdfcc7d4966d
ryusukeyasiro <ryusukeyasiro@gmail.com>
2024-11-12 17:24:28 +0900

    ヘッダーファイル ifndef懐かしい

--- include/push_swap.h
+++ include/push_swap.h
@@ -1,12 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ryusukeyashiro <ryusukeyashiro@student.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/12 17:20:17 by ryusukeyash       #+#    #+#             */
+/*   Updated: 2024/11/12 17:20:23 by ryusukeyash      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdbool.h>
 #include <limits.h>
-
 typedef struct s_node
 {
     int value;
-    int index;
     struct s_node *next;
     struct s_node *pre;
 } t_node ;
@@ -16,13 +30,6 @@ typedef struct s_stack
     t_node *top;
     int size;
 } t_stack;
-
-void free_stack(t_stack **stack);
-int parse_check_and_push(int ac , char **av, t_stack **stack_a , int *count);
-void ft_push_a(t_stack **stack_a , int  num);
-bool ft_double_check(int temp , char **args , int i);
-bool ft_numcheck(char *num);
-
 
 void pa(t_stack **stack_a ,t_stack **stack_b);
 void pb(t_stack **stack_a , t_stack **stack_b);
@@ -36,9 +43,26 @@ void rra(t_stack **stack_a);
 void rrb(t_stack **stack_b);
 void rrr(t_stack **stack_a , t_stack **stack_b);
 
-// void free_stack(t_stack **stack);
-// t_stack *init_stack(void);
-// int pase_check_and_push(int ac , char **av, t_stack **stack_a , int *count);
-// bool ft_numcheck(char *num);
-// bool ft_double_check(int temp , char **args , int i);
+t_stack *ft_init_stack();
+int ft_find_pivot(t_stack **stack , int size);
+void ft_quick_sort(t_stack **stack_a , t_stack **stack_b , int len);
+void sort_algo(t_stack **a , t_stack **b , t_stack **hold , int count);
+void ft_sort_min(t_stack **stack_a ,int count);
+void ft_sort_three(t_stack **stack_a);
+void ft_rotation_sort(t_stack **stack_a , int min);
+void ft_sort_check(int pos, int pos_len , t_stack **stack_a , t_stack **stack_b);
+void ft_insert_sort(t_stack **stack_a , t_stack **stack_b , int num , int count);
+void ft_sort_mid(t_stack **a  , t_stack **b , int count);
 
+//utils
+void free_stack(t_stack **stack);
+int count_stack_len_a(t_stack **stack_a);
+int count_stack_len_a(t_stack **stack_b);
+void ft_push_a(t_stack **stack_a , int  num);
+int stack_b_min(t_stack **stack_b);
+int stack_a_min(t_stack **stack_a);
+bool ft_double_check(int temp , char **args , int i);
+bool ft_numcheck(char *num);
+int ft_parse_check_and_push(int ac , char **av, t_stack **stack_a , int *count);
+
+#endif
