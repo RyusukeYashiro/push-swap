99a0945cb282bd0545ea06ed226f5cae3ae54fe4
ryusukeyasiro <ryusukeyasiro@gmail.com>
2024-11-12 17:24:47 +0900

    print_command消した

--- src/main.c
+++ src/main.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ryusukeyashiro <ryusukeyashiro@student.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/12 17:23:39 by ryusukeyash       #+#    #+#             */
+/*   Updated: 2024/11/12 17:23:40 by ryusukeyash      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../include/push_swap.h"
 
-t_stack *init_stack(void)
+t_stack *ft_init_stack()
 {
     //ヒープ領域にスタック構造体のメモリを確保
     t_stack *new_stack = malloc(sizeof(t_stack));
@@ -10,7 +22,6 @@ t_stack *init_stack(void)
     new_stack -> top = NULL;
     return (new_stack);
 }
-
 
 int main(int ac , char *av[])
 {
@@ -29,19 +40,17 @@ int main(int ac , char *av[])
     //引数の長さを保持しておきたい
     count = 0;
     //errorフラグを立てておく。もし立ったら終了して忘れずfree関数に投げる
-    error_flag = pase_check_and_push(ac , av ,&stack_a , &count);
+    error_flag = ft_pase_check_and_push(ac , av ,&stack_a , &count);
     
     if (error_flag)
     {
-        ft_putstr("Error\n");
+        ft_putstr("Error\n" , 1);
         free_stack(&stack_a);
         free_stack(&stack_b);
         return (1);
     }
     //sortアルゴリズムを実装
     ft_sort_stack(&stack_a , &stack_b , &command_result , count);
-    //sortしたコマンドを出力する
-    print_command(command_result);
     free_stack(&stack_a);
     free_stack(&stack_b);
     return (0);
