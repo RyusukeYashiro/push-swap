dcd7259eec29d9b5ae2ab6427c28cb96c1b7618d
ryusukeyasiro <ryusukeyasiro@gmail.com>
2024-11-12 17:27:21 +0900

    クイックソートの実装

--- src/sort_algo.c
+++ src/sort_algo.c
@@ -1,97 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_algo.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ryusukeyashiro <ryusukeyashiro@student.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/12 17:23:44 by ryusukeyash       #+#    #+#             */
+/*   Updated: 2024/11/12 17:23:56 by ryusukeyash      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../include/push_swap.h"
 
-//連結リストバージョン（もしかしたら使わないかも）
-// t_stack *deepCopy(t_stack *src , t_stack **copy_a)
-// {
-//     t_node *temp;
-//     t_node *newNode;
-//     t_node *copy_temp;
-
-//     if(src == NULL || src -> top == NULL)
-//         return (NULL);
-//     別の関数でcopy_aのノードはつくられてあるからここでは
-//     最初のノードの設定をしている。
-//     (*copy_a) -> top -> value = src -> top -> value;
-//     (*copy_a) -> top -> next = (*copy_a) -> top;
-//     (*copy_a) -> top -> pre = (*copy_a) -> top;
-//     上記により先に進めているため、nextになる。
-//     temp = src -> top -> next;
-//     copy_temp = (*copy_a) -> top;
-//     while(temp != src -> top)
-//     {
-//         newNode = malloc(sizeof(t_node));
-//         if(!newNode)
-//             return(free_stack(*copy_a) , NULL);
-//         新しいノードの値にvalueの値をコピーしておく。
-//         newNode -> value = temp -> value;
-//         newNodeのnext,preの設定
-//         newNode -> next = (*copy_a) -> top;
-//         newNode -> pre = copy_temp;
-//         現在の場所を保存しているcopy_tempのnext,preの設定
-//         copy_temp -> next = newNode;
-//         (*copy_a) -> top -> pre = newNode;
-//         最後のノードを更新
-//         copy_temp = newNode;
-//         temp = temp -> next;
-//     }
-//     retrun(*copy_a);
-// }
-
-//ランダムアクセスを可能にするための関数
-t_node **ft_pointer_buffer(t_stack *src , int count)
+int ft_find_pivot(t_stack **stack , int size)
 {
-    t_node **buffer;
     t_node *temp;
-    t_node *first;
     int i;
+    int count;
+    int middle_num;
 
-    if(src == NULL || src -> top == NULL)
-        return ;
-    buffer = malloc(count * sizeof(t_node*));
-    if(buffer == NULL)
-        return NULL;
-    temp = src -> top;
-    first = src -> top;
+    temp = (*stack) -> top;
+    middle_num = temp -> value;
     i = 0;
-    while(i < count)
+    while(i < size)
     {
-        buffer[i] = temp;
+        count = 0;
+        temp = (*stack) -> top;
+        while(temp -> next != (*stack) -> top)
+        {
+            if(temp -> value < middle_num)
+                count++;
+            temp = temp -> next;
+        }
+        if(count == size / 2)
+            return middle_num;
         temp = temp -> next;
-        if(temp == first)
-            break;
+        middle_num = temp -> value;
         i++;
-    } 
-    return (buffer);
+    }
+    return middle_num;
+}
+
+void ft_quick_sort(t_stack **stack_a , t_stack **stack_b , int len)
+{
+    int pivot;
+    int i;
+    int pb_size;
+
+    pivot = ft_find_pivot(stack_a , len);
+    i = 0;
+    pb_size = 0;
+    while(i < len)
+    {
+        if((*stack_a) -> top -> value < pivot)
+        {
+            pb(stack_a , stack_b);
+            pb_size++;
+        } else
+            ra(stack_a);
+        i++;
+    }
+    ft_quick_sort(stack_b , stack_a , pb_size);
+    ft_quick_sort(stack_a , stack_b , len - pb_size);
+
+    while(pb_size--)
+    {
+        rra(stack_b);
+        pa(stack_a , stack_b);
+    }
 }
 
 void sort_algo(t_stack **a , t_stack **b , t_stack **hold , int count)
 {
-    t_node **array;
-    t_node **array_copy;
     if(count <= 3)
         ft_sort_min(a , count);
     else if (count <= 6)
         ft_sort_mid(a , b);
     else 
-    {
-
-    }
-    // t_node **array;
-    // t_node **array_copy;
-    // //t_stack_aをランダムアクセスできる配列へと変換する関数
-    // array = ft_pointer_buffer(a , count);
-    // array_copy = ft_pointer_buffer(a , count);
-    // //copy_a = init_stack();
-    // //deepCopy(a , &copy_a);
-    // if(count <= 6)
-    // {
-    //     ft_sort_min(a , b, array , count);
-    // }
-    // else if(count >= 7)
-    // {
-    //     array = ft_compress(a,array_copy, count);
-    //     ft_sort_long(array , a , b , hold);
-    // }
-    
-
+        ft_quick_sort(a , b ,count);
 }
