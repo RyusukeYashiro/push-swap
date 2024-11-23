40b92f8dbe913ab9ab98bdf1ce218df0c71bd09c
ryusukeyasiro <ryusukeyasiro@gmail.com>
2024-11-23 20:54:20 +0900

    saを変えた : lastをつけて正しく繋がるようにした

--- src/ft_command/ft_swap.c
+++ src/ft_command/ft_swap.c
@@ -6,7 +6,7 @@
 /*   By: ryusukeyashiro <ryusukeyashiro@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 17:22:03 by ryusukeyash       #+#    #+#             */
-/*   Updated: 2024/11/12 18:20:57 by ryusukeyash      ###   ########.fr       */
+/*   Updated: 2024/11/23 20:53:35 by ryusukeyash      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,43 +17,49 @@ void sa(t_stack **stack_a)
 {
     t_node *first;
     t_node *second;
+    t_node *last;
 
-    if(!stack_a || !(*stack_a) || (*stack_a) -> top -> next == (*stack_a) -> top)
+    if(!stack_a || !(*stack_a) || (*stack_a)->top->next == (*stack_a)->top)
         return;
-    first = (*stack_a) -> top;
-    second = first ->next;
-    //最初の次をnextの次へ
-    first -> next = second -> next;
-    first -> pre = second;
 
-    second -> next = first;
-    second -> pre = first -> pre;
+    first = (*stack_a)->top;
+    second = first->next;
+    last = first->pre;  // 最後のノードを保持
 
-    first -> next -> pre = first;
-    (*stack_a) -> top = second;
+    // 最初の次をnextの次へ
+    first->next = second->next;
+    first->pre = second;
+    second->next = first;
+    second->pre = last;
 
-    ft_putendl_fd("sa" , 1);
+    last->next = second;
+    first->next->pre = first;
+    (*stack_a)->top = second;
+    ft_putendl_fd("sa", 1);
 }
 
 void sb(t_stack **stack_b)
 {
     t_node *first;
     t_node *second;
+    t_node *last;
 
     if(!stack_b || !(*stack_b) || (*stack_b) -> top -> next == (*stack_b) -> top)
         return;
-    first = (*stack_b) -> top;
+    
+    first  = (*stack_b) -> top;
     second = first -> next;
+    last = first -> pre;
 
     first -> next = second -> next;
     first -> pre = second;
-
     second -> next = first;
-    second -> pre = first -> pre;
+    second -> pre = last;
 
+    last -> next = second;
     first -> next -> pre = first;
     (*stack_b) -> top = second;
-
+    
     ft_putendl_fd("sb" , 1);
 }
 
