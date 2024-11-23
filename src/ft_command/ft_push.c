ccc1993b1bf2a8e3ee3c7c1489b9056fbae1a22c
ryusukeyasiro <ryusukeyasiro@gmail.com>
2024-11-23 21:49:39 +0900

    if((*stack_b) -> top == NULL) これが無限ループの原因だった。そりゃそう

--- src/ft_command/ft_push.c
+++ src/ft_command/ft_push.c
@@ -6,76 +6,78 @@
 /*   By: ryusukeyashiro <ryusukeyashiro@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 17:21:01 by ryusukeyash       #+#    #+#             */
-/*   Updated: 2024/11/17 12:33:05 by ryusukeyash      ###   ########.fr       */
+/*   Updated: 2024/11/23 21:46:23 by ryusukeyash      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/push_swap.h"
 #include "../../include/libft.h"
 
-
 void pa(t_stack **stack_a , t_stack **stack_b)
 {
     t_node *node_elem;
-    t_node *first_a;
-
-    if(!stack_b || !(*stack_b) || !(*stack_b) -> top)
+    if(!stack_b || !(*stack_b) || !(*stack_b)->top)
         return;
-    node_elem = (*stack_b) -> top;
-    first_a = (*stack_a) -> top;
-    if(node_elem -> next == node_elem)
-        node_elem = NULL;
+    node_elem = (*stack_b)->top;
+
+    // スタックBの更新
+    if(node_elem->next == node_elem)
+        (*stack_b)->top = NULL;
     else
     {
-        node_elem -> next -> pre = node_elem -> pre;
-        node_elem -> pre -> next = node_elem -> next;
-        node_elem = node_elem ->next;
+        node_elem->next->pre = node_elem->pre;
+        node_elem->pre->next = node_elem->next;
+        (*stack_b)->top = node_elem->next;
     }
-    if((*stack_b) -> top == NULL)
+
+    //からの場合
+    if(!(*stack_a) || !(*stack_a)->top)
     {
-        node_elem -> next = node_elem;
-        node_elem -> pre = node_elem;
+        node_elem->next = node_elem;
+        node_elem->pre = node_elem;
     }
     else
     {
-        node_elem -> next = first_a;
-        node_elem -> pre = first_a -> pre;
-        first_a -> pre -> next = node_elem;
-        first_a -> pre = node_elem;
+        node_elem->next = (*stack_a)->top;
+        node_elem->pre = (*stack_a)->top->pre;
+        (*stack_a)->top->pre->next = node_elem;
+        (*stack_a)->top->pre = node_elem;
     }
     (*stack_a) -> top = node_elem;
-    ft_putendl_fd("pa" , 1);
+    ft_putendl_fd("pa", 1);
 }
 
 void pb(t_stack **stack_a , t_stack **stack_b)
 {
     t_node *node_elem;
-    t_node *first_b;
-
-    if(!stack_a || !(*stack_a) || !(*stack_a) -> top)
+    if(!stack_a || !(*stack_a) || !(*stack_a)->top)
         return;
-    node_elem = (*stack_a) -> top;
-    first_b = (*stack_b) -> top;
-    if(node_elem -> next == node_elem)
-        (*stack_a) -> top = NULL;
-    else 
+    node_elem = (*stack_a)->top;
+
+    // スタックAの更新
+    if(node_elem->next == node_elem)
+        (*stack_a)->top = NULL; // スタックAが空になる場合
+    else
     {
-        node_elem -> next -> pre = node_elem -> pre;
-        node_elem -> pre -> next = node_elem -> next;
-        (*stack_a)->top = node_elem -> next;
+        node_elem->next->pre = node_elem->pre;
+        node_elem->pre->next = node_elem->next;
+        (*stack_a)->top = node_elem->next;
     }
-    if((*stack_b) -> top == NULL)
+
+    // スタックBの更新
+    if(!(*stack_b) || !(*stack_b)->top) // スタックBが空の場合
     {
-        node_elem -> next = node_elem;
-        node_elem -> pre = node_elem;
+        node_elem->next = node_elem;
+        node_elem->pre = node_elem;
+        (*stack_b)->top = node_elem;
     }
     else
     {
-        node_elem -> next = first_b;
-        node_elem -> pre = first_b -> pre;
-        first_b -> pre -> next = node_elem;
-        first_b -> pre = node_elem;
+        node_elem->next = (*stack_b)->top;
+        node_elem->pre = (*stack_b)->top->pre;
+        (*stack_b)->top->pre->next = node_elem;
+        (*stack_b)->top->pre = node_elem;
+        (*stack_b)->top = node_elem;
     }
-    (*stack_b) -> top = node_elem;
-    ft_putendl_fd("pb" , 1);
+    ft_putendl_fd("pb", 1);
 }
