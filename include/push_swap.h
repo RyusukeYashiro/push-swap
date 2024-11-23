33b504bd532f1171bb951e60d9ae9fd7911996b7
ryusukeyasiro <ryusukeyasiro@gmail.com>
2024-11-23 21:47:45 +0900

    ヘッダーファイル更新

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: ryusukeyashiro <ryusukeyashiro@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 17:20:17 by ryusukeyash       #+#    #+#             */
-/*   Updated: 2024/11/17 11:47:40 by ryusukeyash      ###   ########.fr       */
+/*   Updated: 2024/11/23 20:28:05 by ryusukeyash      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,7 +50,7 @@ void ft_quick_sort(t_stack **stack_a , t_stack **stack_b , int len);
 void ft_quick_sort(t_stack **stack_a , t_stack **stack_b , int len);
 void ft_sort_min(t_stack **stack_a ,int count);
 void ft_sort_three(t_stack **stack_a);
-void ft_rotation_sort(t_stack **stack_a , int min);
+void ft_rotation_sort(t_stack **stack_a);
 void ft_sort_check(int pos, int pos_len , t_stack **stack_a , t_stack **stack_b);
 void ft_insert_sort(t_stack **stack_a , t_stack **stack_b , int num);
 void ft_sort_mid(t_stack **a  , t_stack **b , int count);
@@ -60,10 +60,11 @@ void free_stack(t_stack **stack);
 int count_stack_len_a(t_stack **stack_a);
 int count_stack_len_b(t_stack **stack_b);
 void ft_push_a(t_stack **stack_a , int  num);
-int stack_b_min(t_stack **stack_b);
 int stack_a_min(t_stack **stack_a);
+int stack_a_max(t_stack **stack_a);
 bool ft_double_check(int temp , char **args , int current_i);
 bool ft_numcheck(char *num);
 int ft_parse_check_and_push(int ac , char **av, t_stack **stack_a , int *count);
+void print_stack(t_stack **stack_a);
 
 #endif
