d4e0d2cbe9acd8258e35de7ffa83857ba496796f
ryusukeyasiro <ryusukeyasiro@gmail.com>
2024-11-23 18:30:40 +0900

    最大値を見つける関数

---
+++ src/utils/ft_stack_max.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_stack_max.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ryusukeyashiro <ryusukeyashiro@student.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/23 15:53:32 by ryusukeyash       #+#    #+#             */
+/*   Updated: 2024/11/23 18:30:15 by ryusukeyash      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/push_swap.h"
+
+int stack_a_max(t_stack **stack_a)
+{
+    t_node *temp;
+    t_node *first;
+    int max;
+    
+    if (!stack_a || !*stack_a || !(*stack_a)->top)
+        return (0);
+    temp = (*stack_a)->top;
+    first = temp;
+    max = temp->value;
+    temp = temp->next;
+    while (temp != first)
+    {
+        if (temp->value > max)
+            max = temp->value;
+        temp = temp->next;
+    }
+    return (max);
+}
