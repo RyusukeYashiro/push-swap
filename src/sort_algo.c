efec3ab565d2012f03f2e84b43091293c980bfde
ryusukeyasiro <ryusukeyasiro@gmail.com>
2024-11-24 22:31:46 +0900

    クイックソート２つ目　, h のへんしゅう

--- src/sort_algo.c
+++ src/sort_algo.c
@@ -42,7 +42,7 @@ int ft_find_pivot(t_stack **stack , int size)
     return temp -> value;
 }
 
-void ft_quick_sort(t_stack **stack_a , t_stack **stack_b , int len)
+void ft_quick_sort_a(t_stack **stack_a , t_stack **stack_b , int len)
 {
     int pivot;
     int i;
@@ -50,7 +50,7 @@ void ft_quick_sort(t_stack **stack_a , t_stack **stack_b , int len)
 
     if(len <= 3)
     {
-        ft_sort_min(stack_a , len);
+        ft_sort_min_a(stack_a , len);
         return;
     }
     pivot = ft_find_pivot(stack_a , len);
@@ -67,11 +67,12 @@ void ft_quick_sort(t_stack **stack_a , t_stack **stack_b , int len)
             ra(stack_a);
         i++;
     }
+    
     printf("これは再起に入る前の処理です\n");
     print_stack_a(stack_a);
     print_stack_b(stack_b);
-    ft_quick_sort(stack_b , stack_a , pb_size);
-    ft_quick_sort(stack_a , stack_b , len - pb_size);
+    ft_quick_sort_a(stack_a , stack_b , len - pb_size);
+    ft_quick_sort_b(stack_b , stack_a , pb_size);
     while(pb_size--)
     {
         // rra(stack_b);
@@ -79,12 +80,50 @@ void ft_quick_sort(t_stack **stack_a , t_stack **stack_b , int len)
     }
 }
 
+
+
+void ft_quick_sort_b(t_stack **stack_b , t_stack **stack_a , int len)
+{
+    int pivot;
+    int pa_size;
+    int i;
+
+    if(len <= 3)
+    {
+        ft_sort_min_b(stack_b , len);
+        return;
+    }
+    pa_size = 0;
+    pivot = ft_find_pivot(stack_b , len);
+    i = 0;
+    while(i < len)
+    {
+        if((*stack_b)->top ->value <= pivot)
+        {
+            pa(stack_a ,stack_b);
+            pa_size++;
+        }
+        else
+            rb(stack_b);
+        i++;
+    }
+    
+    printf("これは再起に入る前の処理です\n");
+    print_stack_a(stack_a);
+    print_stack_b(stack_b);
+    ft_quick_sort_a(stack_a , stack_b , pa_size);
+    ft_quick_sort_b(stack_b , stack_a , len - pa_size);
+
+    while (pa_size--)
+        pb(stack_b, stack_a);
+}
+
 void ft_sort_stack(t_stack **a , t_stack **b , int count)
 {
     printf("count : %d" , count);
     if(count <= 3)
     {
-        ft_sort_min(a , count);
+        ft_sort_min_a(a , count);
         printf("in min");
     }
     else if (4 <= count && count <= 6)
@@ -93,5 +132,5 @@ void ft_sort_stack(t_stack **a , t_stack **b , int count)
         printf("in mid");
     }
     else 
-        ft_quick_sort(a , b ,count);
+        ft_quick_sort_a(a , b ,count);
 }
